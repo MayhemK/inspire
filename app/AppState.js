0b359a8429d46c0907b5d2749cb173918bcf359c
Ivan <mayhemk57@gmail.com>
2025-03-07 13:59:48 -0700

    making off canvas

--- app/AppState.js
+++ app/AppState.js
@@ -1,10 +1,14 @@
+
 import { Identity } from './Auth/Identity.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
+import { Todo } from './models/Todo.js'
 
 class ObservableAppState extends EventEmitter {
   /** @type {Identity} */
   identity = null
+  /**@type {Todo[]} */
+  todo = []
 
 }
 
--- app/services/TodoService.js
+++ app/services/TodoService.js
@@ -1,10 +1,14 @@
 import { api } from "../utils/Axios.js";
+import { AppState } from "../AppState.js";
+import { Todo } from "../models/Todo.js";
+
 
 class TodoService {
   async getTodo() {
     const response = await api.get('api/todos')
     console.log('got todo', response.data);
-
+    const todos = response.data.map(pojo => new Todo(pojo))
+    AppState.todo = todos
   }
 }
 
